fc198a391f8c563ec86686238ec2c38d6690f383
Stefan <roth.neukirchen@googlemail.com>
2023-10-26 10:37:58 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'da-bubble-2d7db',
+    appId: '1:871979284487:web:25ddd9e679e747de5f12a2',
+    storageBucket: 'da-bubble-2d7db.appspot.com',
+    apiKey: 'AIzaSyBBcawGW_eCP50Ip98HwtbKcG1PMTDzD28',
+    authDomain: 'da-bubble-2d7db.firebaseapp.com',
+    messagingSenderId: '871979284487',
+  },
+};
