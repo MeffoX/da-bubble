aaed4ca7f2676a69f71b133df0289d3775e1b742
davidosi93 <d.osipov93@googlemail.com>
2023-10-30 19:12:02 +0100

    david pusht header und weitere kompnenten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IntroComponent } from './login-components/intro/intro.component';
 import { LoginComponent } from './login-components/login/login.component';
 import { ForgotPasswordComponent } from './login-components/forgot-password/forgot-password.component';
 import { SignUpComponent } from './login-components/sign-up/sign-up.component';
 import { ResetPasswordComponent } from './login-components/reset-password/reset-password.component';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -21,11 +25,15 @@ import { ResetPasswordComponent } from './login-components/reset-password/reset-
     LoginComponent,
     ForgotPasswordComponent,
     SignUpComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
+    MatDialogModule,
+    MatMenuModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
