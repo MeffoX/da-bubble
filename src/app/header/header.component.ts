ca3ed3450db68108b9d1d10b98ee3a2da1143297
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-10-31 17:26:59 +0100

    added logout function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../services/login-service/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private authService: LoginService, private router: Router) {}
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
 }
