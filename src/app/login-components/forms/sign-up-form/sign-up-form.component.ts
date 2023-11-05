4fd6169e9ecf562c07010ba0898e3939cc20c6ef
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-11-05 11:13:56 +0100

    avatar component responsiv

--- src/app/login-components/forms/sign-up-form/sign-up-form.component.ts
+++ src/app/login-components/forms/sign-up-form/sign-up-form.component.ts
@@ -24,7 +24,7 @@ export function passwordMatchValidator(): ValidatorFn {
   styleUrls: ['./sign-up-form.component.scss']
 })
 export class SignUpFormComponent {
-avatarForm = false;
+avatarForm = true;
 
 
   signUpForm = new FormGroup(
