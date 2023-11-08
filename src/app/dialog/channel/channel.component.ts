0339289b2abecd5d9d02220731e88201a8dd1782
davidosi93 <d.osipov93@googlemail.com>
2023-11-08 21:51:37 +0100

    Chanel-Dialog-erstellt

--- src/app/dialog/channel/channel.component.ts
+++ src/app/dialog/channel/channel.component.ts
@@ -1,41 +1,30 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-channel',
   templateUrl: './channel.component.html',
-  styleUrls: ['./channel.component.scss'],
-  animations: [
-    trigger('showHide', [
-      state('show', style({
-        opacity: 1
-      })),
-      state('hide', style({
-        opacity: 0
-      })),
-      transition('show => hide', animate('300ms ease-out')),
-      transition('hide => show', animate('300ms ease-in'))
-    ]),
-    trigger('moveUp', [
-      state('up', style({
-        transform: 'translateY(-100%)'
-      })),
-      state('down', style({
-        transform: 'translateY(0)'
-      })),
-      transition('up => down', animate('300ms ease-out')),
-      transition('down => up', animate('300ms ease-in'))
-    ])
-  ]
+  styleUrls: ['./channel.component.scss']
 })
 export class ChannelComponent {
-  displayNone: boolean = false;
-  displayUpperPart: boolean = true;
   displayInput: boolean = false;
+  displayTextarea: boolean = false;
 
-  constructor() {}
+  constructor(private dialogRef: MatDialogRef<ChannelComponent>) { }
 
-  toggleInput() {
+  toggleChanelName() {
     this.displayInput = !this.displayInput;
+  }
+
+  toggleChanelDescription() {
+    this.displayTextarea = !this.displayTextarea;
+  }
+
+  leaveChanel() {
+    this.closeDialog();
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
   }
 }
