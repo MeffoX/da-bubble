c026e0c2dab451618a4e4dd084c758c22f36580e
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-11 20:21:53 +0100

    setSelectedUser correction

--- src/app/workspace-components/workspace/workspace.component.ts
+++ src/app/workspace-components/workspace/workspace.component.ts
@@ -34,76 +34,76 @@ export class WorkspaceComponent implements OnInit {
     public mainChat: MainChatComponent
   ) {}
 
- /**
- * Initializes the component by subscribing to the user, loading users, and loading channels.
- */
-ngOnInit(): void {
-  // Subscribe to changes in the current user
-  this.authSubscription = this.loginService.currentUser$.subscribe((user) => {
-    this.currentUser = user;
-  });
+  /**
+   * Initializes the component by subscribing to the user, loading users, and loading channels.
+   */
+  ngOnInit(): void {
+    // Subscribe to changes in the current user
+    this.authSubscription = this.loginService.currentUser$.subscribe((user) => {
+      this.currentUser = user;
+    });
 
-  // Load all users
-  this.userService.getAllUsers().subscribe(
-    (users) => {
-      this.users = users;
-    },
-    (error) => {
-      console.error('Error loading users:', error);
-    }
-  );
+    // Load all users
+    this.userService.getAllUsers().subscribe(
+      (users) => {
+        this.users = users;
+      },
+      (error) => {
+        console.error('Error loading users:', error);
+      }
+    );
 
-  // Load all channels
-  this.channelService.getChannels().subscribe((channels) => {
-    this.channels = channels;
-  });
-}
-
-/**
- * Toggles the visibility of the user list.
- */
-toggleList(): void {
-  this.isListHidden = !this.isListHidden;
-}
-
-/**
- * Toggles the visibility of the contacts list.
- */
-toggleContacts(): void {
-  this.isContactHidden = !this.isContactHidden;
-}
-
-/**
- * Opens the dialog to create a new channel.
- */
-openCreateChannelDialog(): void {
-  const currentUser = this.loginService.getUser();
-  if (currentUser) {
-    this.dialog.open(CreateChannelComponent);
-  } 
-}
-
-/**
- * Unsubscribes from the user subscription when the component is destroyed.
- */
-ngOnDestroy(): void {
-  if (this.authSubscription) {
-    this.authSubscription.unsubscribe();
+    // Load all channels
+    this.channelService.getChannels().subscribe((channels) => {
+      this.channels = channels;
+    });
   }
-}
 
-/**
- * Selects a user and updates global variables for the workspace.
- * @param user - The selected user.
- */
-selectUser(user: any): void {
-  this.userService.selectedUser = user;
-  this.globalVariable.openChannelChat = false;
-  this.globalVariable.openThread = false;
-  this.globalVariable.openDM = true;
-  this.globalVariable.openNewMessage = false;
-  this.closeWorkspace();
-}
+  /**
+   * Toggles the visibility of the user list.
+   */
+  toggleList(): void {
+    this.isListHidden = !this.isListHidden;
+  }
+
+  /**
+   * Toggles the visibility of the contacts list.
+   */
+  toggleContacts(): void {
+    this.isContactHidden = !this.isContactHidden;
+  }
+
+  /**
+   * Opens the dialog to create a new channel.
+   */
+  openCreateChannelDialog(): void {
+    const currentUser = this.loginService.getUser();
+    if (currentUser) {
+      this.dialog.open(CreateChannelComponent);
+    }
+  }
+
+  /**
+   * Unsubscribes from the user subscription when the component is destroyed.
+   */
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+
+  /**
+   * Selects a user and updates global variables for the workspace.
+   * @param user - The selected user.
+   */
+  selectUser(user: any): void {
+    this.userService.selectedUser = user;
+    this.globalVariable.openChannelChat = false;
+    this.globalVariable.openThread = false;
+    this.globalVariable.openDM = true;
+    this.globalVariable.openNewMessage = false;
+    this.closeWorkspace();
+  }
 
 /**
  * Opens a channel and updates global variables for the workspace.
@@ -122,23 +122,23 @@ openChannel(channel: any): void {
   this.closeWorkspace();
 }
 
-/**
- * Opens the view for creating a new message.
- */
-openNewMessage(): void {
-  this.globalVariable.openChannelChat = false;
-  this.globalVariable.openThread = false;
-  this.globalVariable.openDM = false;
-  this.globalVariable.openNewMessage = true;
-  this.closeWorkspace();
-}
-
-/**
- * Closes the workspace if the window width is less than or equal to 1000 pixels.
- */
-closeWorkspace(): void {
-  if (window.innerWidth <= 1000) {
-    this.globalVariable.openWorkspace = false;
+  /**
+   * Opens the view for creating a new message.
+   */
+  openNewMessage(): void {
+    this.globalVariable.openChannelChat = false;
+    this.globalVariable.openThread = false;
+    this.globalVariable.openDM = false;
+    this.globalVariable.openNewMessage = true;
+    this.closeWorkspace();
   }
-}
+
+  /**
+   * Closes the workspace if the window width is less than or equal to 1000 pixels.
+   */
+  closeWorkspace(): void {
+    if (window.innerWidth <= 1000) {
+      this.globalVariable.openWorkspace = false;
+    }
+  }
 }
